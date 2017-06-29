38f48ac2c82125d04048407cf131cadeacae87d3
mrsteele <matt@omnionline.us>
2017-06-29 08:48:17 -0400

    chore: Temporarily forcing tests to fail

--- test/index.spec.js
+++ test/index.spec.js
@@ -9,6 +9,12 @@ import trosjs from '../src'
 const expect = chai.expect
 
 describe('index', () => {
+  describe('fail a test', () => {
+    it('should fail', () => {
+      expect(false).to.be.true
+    })
+  })
+
   describe('default', () => {
     it('should be a function', () => {
       expect(trosjs).to.be.a('function')
